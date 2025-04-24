dc8be5977d05e5c405f6303cd8c1344411d62339
echimine <eliott.chiminello@gmail.com>
2025-04-24 15:01:56 +0200

    added /projets /a-propos /contact + MenuDesktop

--- app/components/Header/MenuDesktop.vue
+++ app/components/Header/MenuDesktop.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="flex gap-6">
-    <Typographie>Projets</Typographie>
-    <Typographie>A propos</Typographie>
-    <Typographie>Contact</Typographie>
+    <NuxtLink to="/projets" :class="getLinkClass('/projets')">
+      Projets
+    </NuxtLink>
+
+    <NuxtLink to="/a-propos" :class="getLinkClass('/a-propos')">
+      A propos
+    </NuxtLink>
+
+    <NuxtLink to="/contact" :class="getLinkClass('/contact')">
+      Contact
+    </NuxtLink>
+
     <SwitchColor />
   </div>
 </template>
 
 <script setup lang="ts">
 import SwitchColor from '../Buttons/SwitchColor.vue';
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const getLinkClass = (targetPath: string) => {
+  return route.path === targetPath
+    ? 'underline text-blue dark:text-orange font-Inter font-normal'
+    : 'font-Inter font-normal dark:text-white text-black';
+};
 </script>
